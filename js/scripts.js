d72f95c5ec3db32e4ca8b24230e21980eaee84e2
atembamanu <atembaemm07@gmail.com>
2019-09-02 23:25:00 +0300

    create a time function to downcount in order  to expire a session

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+let countDownDate = new Date("Sep 2, 2019 23:25:19").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  $(document).ready(function () {
+    $('#days').text(days);
+    $('#hours').text(hours);
+    $('#mins').text(minutes);
+    $('#secs').text(seconds);
+  })
+
+  console.log(distance)
+  if (minutes == 5) {
+    $('#skillSetModal').modal('show');
+  }
+
+  if (distance < 0) {
+    clearInterval(x);
+    $('button#time-elapsed').text('SESSION DESTROYED')
+
+  }
+}, 1000);
