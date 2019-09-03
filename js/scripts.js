d2eefa63d55aabcb208130a2aa0f56bdbde90967
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-09-03 11:54:08 +0300

    show and hide skill set description

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $('#skills-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var recipient = button.data('whatever') // Extract info from data-* attributes    
     var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
-  })
+})
+
+$("img#proceed").click(function () {
+
+    $("#skilltest").show(2500);
+    $(".why-skills").hide(2500);
+})
